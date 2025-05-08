27bbf74b384dee5e3b7a98a54194641ad1738c5f
Yogesh78279 <102730458+Yogesh78279@users.noreply.github.com>
2025-05-08 11:52:50 +0530

    Update HeyController.java

--- src/main/java/com/gitHub/Hello/controller/HeyController.java
+++ src/main/java/com/gitHub/Hello/controller/HeyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HeyController {
 	
-	@GetMapping("/hey/name")
+	@GetMapping("/hey/{name}")
 	public String getHey(@PathVariable("name") String name) {
 		return "Hey	" + name;
 	}
