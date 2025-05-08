75355edf1e57dccdc3748d7ed0edcd38857b7974
Yogesh78279 <102730458+Yogesh78279@users.noreply.github.com>
2025-05-08 11:48:49 +0530

    Update HelloController.java

--- src/main/java/com/gitHub/Hello/controller/HelloController.java
+++ src/main/java/com/gitHub/Hello/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.gitHub.Hello.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
